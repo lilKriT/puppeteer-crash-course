f97f1e0d1474278006718c3715950ab063b3d6d2
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-14 21:00:28 +0100

    Added some examples.

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ async function run() {
   //   await page.screenshot({ path: "example.png", fullPage: true });
   //   await page.pdf({ path: "example.pdf", format: "A4" });
 
+  //   gets the whole page content
+  //   const html = await page.content();
+
+  //   This will just get the title
+  //   const title = await page.evaluate(() => document.title);
+
+  //   Just the text
+  //   const text = await page.evaluate(() => document.body.innerText);
+
+  //   This will get all the links
+  //   const links = await page.evaluate(() =>
+  //     Array.from(document.querySelectorAll("a"), (el) => el.href)
+  //   );
+
   await browser.close();
 }
 run();
